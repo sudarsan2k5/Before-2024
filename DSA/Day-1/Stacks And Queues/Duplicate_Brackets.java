5c2c103d647d81ccf5e6400b8d8ff230b89f5805
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-11-14 23:32:28 +0530

    Balanced Brackets

---
+++ DSA/Day-1/Stacks And Queues/Duplicate_Brackets.java
@@ -0,0 +1,16 @@
+// import java.util.Scanner;
+// import java.util.Stack;
+
+// public class Duplicate_Brackets {
+//     public static void main(String[] args) {
+//         Scanner scn = new Scanner(System.in);
+//         String str = scn.nextLine();
+
+//         Stack<Character> st = new Stack();
+
+//         for(int i = 0; i < str.length(); i++){
+//             char ch = str.charAt();
+//         }
+//     }
+    
+// }
