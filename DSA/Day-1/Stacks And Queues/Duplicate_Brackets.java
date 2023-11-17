c8f6877bca8c502814c7e868a40ec6f27388c32b
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-11-17 20:31:07 +0530

    Duplicate Brackets

--- DSA/Day-1/Stacks And Queues/Duplicate_Brackets.java
+++ DSA/Day-1/Stacks And Queues/Duplicate_Brackets.java
@@ -1,16 +1,28 @@
-// import java.util.Scanner;
-// import java.util.Stack;
+/**
+ * Duplicate_Brackets
+ */
+import java.util.*;
+public class Duplicate_Brackets {
 
-// public class Duplicate_Brackets {
-//     public static void main(String[] args) {
-//         Scanner scn = new Scanner(System.in);
-//         String str = scn.nextLine();
-
-//         Stack<Character> st = new Stack();
-
-//         for(int i = 0; i < str.length(); i++){
-//             char ch = str.charAt();
-//         }
-//     }
-    
-// }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        Stack<Character> stk = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            char chr = str.charAt(i);
+            if(chr == ')'){
+                if(stk.peek() == '('){
+                    System.out.print(true);
+                    return;
+                }else{
+                    while(stk.peek() != '('){
+                        stk.pop();
+                    }
+                }
+            }else{
+                stk.push(chr);
+            }
+        }
+        System.out.print(false);
+    }
+}
